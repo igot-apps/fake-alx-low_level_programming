331e15ee3f0512143f7b1d6e88dc728dd3798ca1
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-15 05:43:50 -0700

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
+ * main -  Calculates and prints the sum of the even-valued terms,
+ * followed-by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
