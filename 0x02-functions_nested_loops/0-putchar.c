40f780eaa6adac566f0fd3a846557d0d1bc298c7
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-15 05:16:23 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
+ * main -  Outputs _putchar and then moves to a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
