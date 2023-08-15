c60da8784e3af630b06eb12594f7ed6f5b600486
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-15 05:18:23 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * Prints the lowercase alphabet ten times consecutively.
  * followed by a new line
  */
 void print_alphabet_x10(void)
