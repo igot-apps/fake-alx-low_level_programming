d05f37fc997e3fefbaffb45f5e9bc4c114211d14
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-17 02:48:13 -0700

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
+ * _isupper - function to verify if a character is in uppercase
+ * @c: the character to be examined
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
