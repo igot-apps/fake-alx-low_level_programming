0e4a5dae26df0a8956c45d22f0b990ae9bcbccf7
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-17 02:51:23 -0700

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * @a: int to be multiplied to b
- * @b: int to be multiplied to a
+ * mul - multipliespmtwopmintegers
+ * @a: int topmbepmmultipliedpmtopmb
+ * @b: int topmbe multipliedpmto a
  * Return: the result of the operation
  */
 int mul(int a, int b)
