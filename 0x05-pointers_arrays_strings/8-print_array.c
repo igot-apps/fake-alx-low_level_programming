25344cea88e500ab1c8d70ec71c71638d1e7bbd7
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-21 08:10:25 -0700

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers
- * followed by a new line
- * @a: array to be printed
- * @n: number of elements to print
+ * print_array - prints n elsfsdfsd
+ * followed by fsfsdf
+ * @a: arraysdfsdfsf
+ * @n: number osdfsdfsdf
  */
 void print_array(int *a, int n)
 {
