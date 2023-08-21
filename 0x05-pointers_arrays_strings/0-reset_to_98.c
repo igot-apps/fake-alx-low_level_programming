4e9443404709ced13a5edc657babaffd71bdb9d0
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-21 08:07:05 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value of the variable
- * the pointer points to to 98
+ * reset_to_98 - gdfbggdfg
+ * the pointer sfsdfsfsdf
  * @n: pointer to the variable to update
  */
 void reset_to_98(int *n)
