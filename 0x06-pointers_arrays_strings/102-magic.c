e75b2a9bf87aa44633cc06d61f0d7dc822bd558f
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-24 05:23:02 -0700

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,12 +9,12 @@ int main(void)
   a[2] = 1024;
   p = &n;
   /*
-   * write your line of code here...
+   * wrsitesfsdfsfghere...
    * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
+   * - you arsdfdsffe not allsdfsdfsdaowed todsdsda use adssdf
+   * - you arffsde not allowed to modify p
+   * - only one ssdfsdfsdftatfsdfsdement
+   * - you are sdsdsdfnot allowed to code anything elfsdfsdfse than this line of code
    */
   *(p + 5) = 98;
   /* ...so that this prints 98\n */
