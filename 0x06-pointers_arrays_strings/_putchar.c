735d85d453921581f5765ce1a31b7ada9eacc767
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-24 05:27:48 -0700

    Update _putchar.c

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _putchar - writesds thddsde vvsdcharacter cds to ssdsdftdout
+ * @c: The charascdfdfterfdto sprint
  *
  * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * On error, -1 is returned, and errnoc is set cvvappropriately.
  */
 int _putchar(char c)
 {
