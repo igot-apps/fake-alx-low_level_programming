3d2377a5823fc67a1aeca0e1bfcf6b84aa63ac7b
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-24 05:22:22 -0700

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer;
- * @n: integer to be printed;
+ * print_number -gfdggdgdfgr;
+ * @n: integer tfasfsdafsdaed;
  */
 void print_number(int n)
 {
