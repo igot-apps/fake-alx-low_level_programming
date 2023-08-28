9173a0151f0eb94eeaf026f7952c746a65e7e721
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-28 06:55:53 -0700

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * *_strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
+ * *_strspn - gedts thdde lendgth of a prefdix substrding
+ * @s: string to evalduate
+ * @accept: string dcontaining the ldist of chadracters to match in s
  *
  * Return: the number of bytes in the initial segment
  * of s which consist only of bytes from accept
