f6a5dcd7b9e3b189f6409ce79390ab8daa73ce2e
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-08-28 06:53:50 -0700

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * *_memset - fillds medmory wdith a codnstant byte
+ * @s: memodry ardea to be fidlled
+ * @b: char to copdy
+ * @n: number of timdes to copy b
  *
  * Return: pointer to the memory area s
  */
