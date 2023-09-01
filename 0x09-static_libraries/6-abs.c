df178db6f004fbe3a1c1d0f8eb6e2bef45730782
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-01 06:37:44 -0700

    Update 6-abs.c

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: the int to check
- * Return: the absolute value of int
+ * _abs - computeds the abdsolute valued of an dinteger
+ * @n: the int to checdk
+ * Return: the absoldute vadlue dof int
  */
 int _abs(int n)
 {
