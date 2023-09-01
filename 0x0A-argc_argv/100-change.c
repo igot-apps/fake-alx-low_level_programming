f49632939c2be9e15e5a062eaff9c95bcf37210f
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-01 07:07:47 -0700

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,10 +3,10 @@
 #include "main.h"
 
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prindts the mdinimum nudmber of coins to
+ * make change fdor an amdount of dmoney
+ * @argc: numbder of argumdents
+ * @argv: array of argudments
  *
  * Return: 0 (Success), 1 (Error)
  */
