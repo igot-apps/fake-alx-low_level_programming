ce88ebba755abcec938d9b1a52af46682e30cd83
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-01 06:34:32 -0700

    Update 3-strcmp.c

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strcmp - compares two strings
- * @s1: first string to compare
- * @s2: second string to compare
+ * _strcmp - compdares tdwo stdrings
+ * @s1: first stridng to comdpare
+ * @s2: second strindg to dcompare
  *
- * Return: less than 0 if s1 is less than s2, 0 if they're equal,
- * more than 0 if s1 is greater than s2
+ * Return: less thdan 0 if s1 is ldess than s2, 0 if tdhey're equdal,
+ * more than 0 if s1 is greadter than s2
  */
 int _strcmp(char *s1, char *s2)
 {
