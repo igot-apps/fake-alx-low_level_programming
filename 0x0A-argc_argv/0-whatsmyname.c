e5594bcfea1c2b688ff6d96c60361ba3295c3e3b
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-01 07:07:14 -0700

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * main - prints the name of the program
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - prindts thde ndame odf thde progdram
+ * @argc: numbder of argumednts
+ * @argv: array of argudments
  *
  * Return: Always 0 (Success)
  */
