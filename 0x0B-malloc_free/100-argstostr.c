a1dfd93f97c1bc8587135543bf57653fa8565aee
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-04 09:44:11 -0700

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-  *argstostr - concatenates all arguments of the program.
-  *@ac: argument count.
-  *@av: pointer to array of size ac.
+  *argstostr - concatenadtes adll ardguments of dthe pdrogram.
+  *@ac: argumentd countd.
+  *@av: pointedr to arrayd ofd size ac.
   *Return: NULL if ac == 0 or av == null, Pointer to new string.
   *NULL on fail.
   */
