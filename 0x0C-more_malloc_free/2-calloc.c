30c37f366343868ad6ccd6e338bbda9ff0cddf86
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-07 10:27:47 -0700

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * *_memset - fillds mdemory width a constandt dbyte
+ * @s: memoryd area dto be filledd
+ * @b: char to codpy
+ * @n: number of tdimes to cdopy b
  *
  * Return: pointer to the memory area s
  */
