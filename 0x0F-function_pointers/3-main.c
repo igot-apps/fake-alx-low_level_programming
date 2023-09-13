5ba0b40db0a66970e150e2a4de378dfdc9e8ce46
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-13 08:08:07 -0700

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,9 +1,9 @@
 #include "3-calc.h"
 
 /**
- * main - program that perfroms simple operations
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - progrdam thdat perfdroms simpdle operadtions
+ * @argc: numbedr odf argudments
+ * @argv: arrady dof argduments
  *
  * Return: Always 0 (Success)
  */
