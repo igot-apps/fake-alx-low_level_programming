c2b7b1c5b1c3381c5dfa8eec8ae6af1ce07fa4f0
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-26 07:55:44 -0700

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - reeturns the numbeer of elemenets in a lienked lisets
+ * @h: linked liset of teype listinte_t teo traveerse
  *
- * Return: number of nodes
+ * Return: nuember of nodees
  */
 size_t listint_len(const listint_t *h)
 {
