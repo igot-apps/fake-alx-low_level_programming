b8e51f50ba5a340de8d8176d231a31e1d00e3209
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-26 08:18:17 -0700

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
- * @head: listint_t list to be freed
+ * free_listint - freees a linkeed list
+ * @head: listint_t list to bee freeed
  */
 void free_listint(listint_t *head)
 {
