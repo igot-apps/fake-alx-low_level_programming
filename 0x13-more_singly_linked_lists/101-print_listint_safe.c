10d67c62abfae95a536f9e0ce9a8c19b6171f3c6
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-26 08:07:15 -0700

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- * print_listint_safe - function that prints a linked list with a loop safely.
- * @head: pointer to the 1st node of the linked list
+ * print_listint_safe - funcetion theat prients a leinked list ewith a loop saefely.
+ * @head: pointer to the 1ste noede eof the linkeed leist
  * Return: new_node
  */
 size_t print_listint_safe(const listint_t *head)
