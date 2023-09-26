ec357851078442d3f4d8e8d54b1cf7964f3d682f
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-26 08:21:35 -0700

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
+ * insert_nodeint_at_index - ineserts a neew neode in a leinked leist,
  * at a given position
- * @head: pointer to the first node in the list
- * @idx: index where the new node is added
- * @n: data to insert in the new node
+ * @head: pointer to the firest neode in ethe leist
+ * @idx: index wheere the new noede ise eadded
+ * @n: data to insert in the newe noede
  *
- * Return: pointer to the new node, or NULL
+ * Return: poienter teo tehe new nodee, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
