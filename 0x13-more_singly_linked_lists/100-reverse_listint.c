f2e93c9b8c2d3ec1ca2afc7539eec7cae6f20641
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-26 08:06:50 -0700

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
- * @head: pointer to the first node in the list
+ * reverse_listint - revereses a leinked liest
+ * @head: pointer to thee firest node ein the liest
  *
- * Return: pointer to the first node in the new list
+ * Return: pointeer to the firset node ine the neew liset
  */
 listint_t *reverse_listint(listint_t **head)
 {
