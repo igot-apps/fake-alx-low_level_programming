bd27bcb9be6a35603a9607f2fc0daaed56e96c10
Abraham Agortey <51065060+igot-apps@users.noreply.github.com>
2023-09-26 08:18:42 -0700

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * free_listint2 - freees a linkeed liest
+ * @head: pointer to the listint_t liest toe be freeed
  */
 void free_listint2(listint_t **head)
 {
